c8941569e5590747e976f60af89661382a548b71
timsliu <timliu92@gmail.com>
2019-03-19 23:15:59 -0700

    created flight_time.py, added particle constants to astro_constant. Added to section 3 and 4 of appendix.

--- astro_constants.py
+++ astro_constants.py
@@ -7,6 +7,7 @@
 # 03/18/19    Tim Liu    copied constants from oberth
 
 
+# orbital constants
 G = 6.674e-11                         # universal gravity constant in MKS
 M_S = 1.99e30                         # mass of the sun in kilogram
 R_S = 6.96e8                          # radius of the sun in meters
@@ -14,3 +15,12 @@ C = 299792458                         # speed of light meters per second
 AU = 1.496e11                         # astronomical unit in meters
 JUPITER_R = 5 * AU                    # approximate Jupiter's distance from sun in meters (5AU)
 JUPITER_V = (G*M_S/JUPITER_R) ** 0.5  # approximate orbital velocity of Jupiter
+
+# particle constants
+J_PER_EV = 1.602e-19                  # joules per electron volt
+MASS_HE4 = 6.644e-27                  # mass of He-4 in kg
+MASS_HE3 = 5.008e-27                  # mass of He-3 in kg
+MASS_D2 =  3.343e-27                  # mass of deuterium in kg
+MASS_T3 =  5.008e-27                  # mass of tritium in kg
+MASS_P1 =  1.673e-27                  # mass of proton in kg
+MASS_N1 =  1.675e-27                  # mass of neutron in kg
--- flight_time.py
+++ flight_time.py
@@ -3,19 +3,29 @@
 # program for calculating the flight time to the focal point
 #
 # Table of contents
+# calc_exhaust_velocity - calculates the exhausts velocity from a particle's
+#                         energy and mass
+# calc_flight_time - calculates the flight time of Einstein
 #
 # Revision history
+# 03/19/19    Tim Liu    created file and wrote calc_exhaust_velocity 
 #
 
 from astro_constants import *
 
-def calc_exhaust_velocity(mass, energy):
+import math
+
+def calc_exhaust_velocity(mass, energy_ev):
     '''calculates the exhaust velocity of a particle
     inputs: mass - mass of the particle
-            energy - kinetic energy of the particle
+            energy - kinetic energy of the particle in MeV
     outputs: ve - exhaust velocity in meters per second'''
 
-    return
+    energy_ev *= 1000000                    # convert MeV to EV
+
+    energy_j = energy_ev * J_PER_EV         # convert energy to joules
+    velocity = math.sqrt(2 * energy_j/mass) # calculate velocity in m/s
+    return velocity
 
 
 # TODO
